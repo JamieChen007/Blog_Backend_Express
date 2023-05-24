471958ed02e25a3216ac2407f4834a7ca43b6056
JamieChen007 <107536976+JamieChen007@users.noreply.github.com>
2023-05-24 11:35:23 +1000

    update format

--- app.js
+++ app.js
@@ -22,14 +22,14 @@ app.set('view engine', 'jade');
 
 const ENV = process.env.NODE_ENV
 
-if(ENV !== 'production'){
+if (ENV !== 'production') {
   app.use(logger('dev'));
 } else {
-  const fullFileName = path.join(__dirname,'logs','access.log')
-  const writeStream = fs.createWriteStream(fullFileName,{
-    flags:'a'
-})
-  app.use(logger('combined',{
+  const fullFileName = path.join(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(fullFileName, {
+    flags: 'a'
+  })
+  app.use(logger('combined', {
     stream: writeStream
   }));
 }
@@ -42,14 +42,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const redisClient = require('./db/redis')
 const sessionStore = new RedisStore({
-  client:redisClient
+  client: redisClient
 })
 app.use(session({
   secret: 'Abc123+-',
   cookie: {
     // path : '/',
     // httpOnly : true,
-    maxAge : 24*60*60*1000
+    maxAge: 24 * 60 * 60 * 1000
   },
   store: sessionStore
 }))
@@ -60,12 +60,12 @@ app.use('/api/blog', blogRouter);
 app.use('/api/user', userRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -73,7 +73,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-  
+
 });
 
 module.exports = app;
--- routes/blog.js
+++ routes/blog.js
@@ -5,8 +5,8 @@ const { getList,
   newBlog,
   updateBlog,
   delBlog
-  } = require('../controller/blog')
-const { SuccessModel,ErrorModel } = require('../model/resModel')
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 const loginCheck = require('../middleware/loginCheck')
 
@@ -14,11 +14,11 @@ const loginCheck = require('../middleware/loginCheck')
 router.get('/list', (req, res, next) => {
   var author = req.query.author || ''
   const keyword = req.query.keyword || ''
-  
-if(req.query.isadmin){
+
+  if (req.query.isadmin) {
     console.log('is admin')
-    
-    if(req.session.username == null){
+
+    if (req.session.username == null) {
       console.error('is admin,but no login')
       res.json(
         new ErrorModel('no login')
@@ -28,98 +28,98 @@ if(req.query.isadmin){
     author = req.session.username
   }
 
-  const result = getList(author,keyword)
-  return result.then(listData=>{
-      res.json(new SuccessModel(listData))
+  const result = getList(author, keyword)
+  return result.then(listData => {
+    res.json(new SuccessModel(listData))
   })
 
   // res.json({
   //   result: 'OK',
   //   data: 1
   // }
-    
+
   // )
 });
 
-router.get('/detail',(req, res, next) => {
-  
-  const id = req.query.id || '' 
+router.get('/detail', (req, res, next) => {
+
+  const id = req.query.id || ''
   const result = getDetail(id)
-    return result.then(detailData=>{
+  return result.then(detailData => {
+    res.json(
+      new SuccessModel(detailData)
+    )
+  })
+})
+
+router.get('/search', (req, res, next) => {
+  let { isadmin = "", author = "", keyword = "" } = req.query;
+  if (isadmin) {
+    author = req.session.username;
+  }
+
+  const result = getList(author, keyword);
+  return result
+    .then((data) => {
       res.json(
-        new SuccessModel(detailData)
-        )
+        new SuccessModel(data, "list blog success")
+      )
     })
+    .catch((error) => {
+      res.json(
+        new ErrorModel("list blog fail:" + error)
+      )
+    });
 })
 
-router.get('/search',(req, res, next) => {
-    let { isadmin = "", author = "", keyword = "" } = req.query;
-    if (isadmin) {
-      author = req.session.username;
+router.post('/create', loginCheck, (req, res, next) => {
+
+  req.body.author = req.session.username
+  const result = newBlog(req.body)
+
+  return result.then(newBlog => {
+    res.json(
+      new SuccessModel(newBlog)
+    )
+  })
+})
+
+router.post('/update', loginCheck, (req, res, next) => {
+
+  const id = req.query.id || ''
+  const result = updateBlog(id, req.body)
+
+  return result.then(val => {
+    if (val) {
+      res.json(
+        new SuccessModel()
+      )
+    } else {
+      res.json(
+        new ErrorModel('update blog fail')
+      )
+
     }
-    
-    const result = getList(author, keyword);
-    return result
-      .then((data) => {
-        res.json(
-          new SuccessModel(data, "list blog success")
-          )
-      })
-      .catch((error) => {
-        res.json(
-          new ErrorModel("list blog fail:" + error)
-          )
-      });
+  })
 })
 
-router.post('/create', loginCheck,  (req,res,next) => {
-      
-      req.body.author = req.session.username
-      const result = newBlog(req.body)
+router.post('/delete', loginCheck, (req, res, next) => {
 
-      return result.then(newBlog=>{
-          res.json(
-            new SuccessModel(newBlog)
-          ) 
-      })
-})
+  const id = req.query.id || ''
+  const author = req.session.username
+  const result = delBlog(id, author)
 
-router.post('/update', loginCheck, (req,res,next) => {
-    
-      const id = req.query.id || '' 
-      const result = updateBlog(id, req.body)
-
-      return result.then(val => {
-          if(val){
-            res.json(
-              new SuccessModel()
-            ) 
-          }else{
-            res.json(
-              new ErrorModel('update blog fail')
-            )
-              
-          }
-      })
-})
-
-router.post('/delete', loginCheck, (req,res,next) => {
-    
-      const id = req.query.id || '' 
-      const author=req.session.username
-      const result = delBlog(id,author)
-
-      return result.then(val => {
-          if(val){
-              res.json(
-                new SuccessModel()
-              ) 
-          }else{
-            res.json(
-              new ErrorModel('delete blog fail')
-              )
-          }
-      })
+  return result.then(val => {
+    if (val) {
+      res.json(
+        new SuccessModel()
+      )
+    } else {
+      res.json(
+        new ErrorModel('delete blog fail')
+      )
+    }
+  })
 })
 
 module.exports = router;
